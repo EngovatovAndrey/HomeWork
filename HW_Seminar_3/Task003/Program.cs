880f0c2c6cdcef56334cbab1a7a60253f58459da
Andrey Engo <5522662@gmail.com>
2022-12-23 16:04:02 +0300

    add HW3 cube matrix

---
+++ HW_Seminar_3/Task003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите число");
+int n = Convert.ToInt32(ReadLine());
+int x = 1;
+
+while (x < n+1)
+{
+    Write($"{Math.Pow(x, 3)}, ");
+    x +=1;
+}
+
+//Write(Math.Pow(n, 3));

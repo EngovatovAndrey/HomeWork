c0ed48f60a14f7f7e34772c5b2f36075de117c20
Andrey Engo <5522662@gmail.com>
2023-01-28 00:35:19 +0300

    add Task002

--- HW_Seminar_8/Task001/Program.cs
+++ HW_Seminar_8/Task001/Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+using static System.Console;
+Clear();
+
+int[,] matrix = GetMatrix2D(4, 5);
+PrintMatrix(matrix);
+WriteLine();
+PrintMatrix(ChangeMatrix(matrix));
+
+int[,] GetMatrix2D(int lines, int column)
+{
+    int[,] mtx = new int[lines, column];
+    for (int i = 0; i < lines; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            mtx[i, j] = new Random().Next(1,10);
+        }
+    }
+    return mtx;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Write($"{matrix[i, j],3} ");
+        }
+        WriteLine();
+    }
+}
+
+int[,] ChangeMatrix(int[,] inArray)
+{
+
+    int[,] newArray = new int[inArray.GetLength(0), inArray.GetLength(1)];
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+                for (int k = 0; k < inArray.GetLength(1) - j - 1; k++)
+                {
+                    if (inArray[i, k] < inArray[i, k + 1])
+                    {
+
+                        int t = inArray[i, k];
+
+                        inArray[i, k] = inArray[i, k + 1];
+
+                        inArray[i, k + 1] = t;
+
+                    }
+                }
+
+            }
+
+        }
+        return inArray;
+    }
+
+
+
---
+++ HW_Seminar_8/Task002/Program.cs
@@ -0,0 +1,68 @@
+﻿/*
+Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+using static System.Console;
+Clear();
+
+int[,] matrix = GetMatrix2D(4, 4);
+PrintMatrix(matrix);
+WriteLine();
+FindMinSumLine(matrix);
+
+int[,] GetMatrix2D(int lines, int column)
+{
+    int[,] mtx = new int[lines, column];
+    for (int i = 0; i < lines; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            mtx[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return mtx;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Write($"{matrix[i, j],2} ");
+        }
+        WriteLine();
+    }
+}
+
+void FindMinSumLine(int[,] matrix)
+{
+    int index = 0, minsum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        if (i == 0)
+        {
+            minsum = sum;
+            index = 1;
+        }
+        else if (sum < minsum)
+        {
+            minsum = sum;
+            index = i+1;
+        }
+    }
+    WriteLine($"Номер строки с наименьшей суммой элементов: {index} строка.");
+}
+

49a9795fb4831b745f281e098e638f4f5f84032c
Andrey Engo <5522662@gmail.com>
2023-01-29 01:15:27 +0300

    add Task004

--- HW_Seminar_8/Task004/Program.cs
+++ HW_Seminar_8/Task004/Program.cs
@@ -1,5 +1,6 @@
 ﻿/*
-Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 Массив размером 2 x 2 x 2
 66(0,0,0) 25(0,1,0)
 34(1,0,0) 41(1,1,0)
@@ -10,42 +11,65 @@
 using static System.Console;
 Clear();
 
-int[,,] matrix = GetMatrix2D(2, 2, 2);
+int[,,] matrix = GetMatrix3D(2, 2, 2);
+PrintMatrix3D(matrix);
 
-
-
-PrintMatrix(matrix);
-WriteLine();
-PrintMatrix(matrix2);
-WriteLine();
-PrintMatrix(DivMatrix(matrix, matrix2));
-
-int[,] GetMatrix2D(int lines, int column)
+int[,,] GetMatrix3D(int x, int y, int z)
 {
-    int[,] mtx = new int[lines, column];
-    for (int i = 0; i < lines; i++)
+    int[] array = new int[90];
+    int e = 0;
+    for (int i = 10; i < 100; i++)
     {
-        for (int j = 0; j < column; j++)
+        array[e] = i;
+        e++;
+    }
+    int[] randomBase = Shuffle(array);
+
+    int[,,] mtx = new int[x, y, z];
+    int index = 0;
+
+    for (int i = 0; i < mtx.GetLength(0); i++)
+    {
+        for (int j = 0; j < mtx.GetLength(1); j++)
         {
-            mtx[i, j] = new Random().Next(1, 10);
+            for (int k = 0; k < mtx.GetLength(2); k++)
+            {
+                //mtx[i, j, k] = new Random().Next(10, 99); 
+                mtx[i, j, k] = randomBase[index];
+                index++;
+            }
         }
     }
     return mtx;
 }
 
-void PrintMatrix(int[,] matrix)
+void PrintMatrix3D(int[,,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Write($"{matrix[i, j],2} ");
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Write("{0}", string.Join(", ", matrix[i, j, k]));
+                Write($"({i}, {j}, {k}) ");
+            }
+            WriteLine();
         }
-        WriteLine();
     }
 }
 
-int[,] DivMatrix(int[,] array, int[,] array2)
+int[] Shuffle(int[] array)
 {
-    
+    var random = new Random();
+    for (var i = 0; i < array.Length; i++)
+    {
+        var key = array[i];
+        var rnd = random.Next(i, array.Length);
+        array[i] = array[rnd];
+        array[rnd] = key;
+    }
+    return array;
 }
+
+

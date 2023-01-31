e067ac42ba1155a715a92544274a396f78667c98
Andrey Engo <5522662@gmail.com>
2023-01-31 11:43:52 +0300

    add Task001

---
+++ HW_Seminar_9/Task001/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 64: Напишите программу, которая реализует рекурсивный метод вывода всех натуральных числел 
+в промежутке от N до 1.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+using static System.Console;
+Clear();
+
+WriteLine("Задайте число, N");
+int userNumber = Convert.ToInt32(ReadLine()!);
+WriteLine(PrintNumbersRec(userNumber));
+
+string PrintNumbersRec (int n)
+{
+    return n == 1? n.ToString(): $"{n}, {PrintNumbersRec (n -1)}";
+}
+

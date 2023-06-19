2851b8ff6103be7f7293a2206751eae5648c9b90
Andrey Engo <5522662@gmail.com>
2023-06-19 18:01:50 +0700

    add HW5

---
+++ HW_JAVA_OOP/HW5_08_06_2023/service/UserService.java
@@ -0,0 +1,19 @@
+package HW5_08_06_2023.service;
+
+import HW5_08_06_2023.model.User;
+
+import java.util.List;
+
+public interface UserService<T extends User> {
+    void create(String fullName, Integer age, String phoneNumber, String nameGroup);
+    List<T> getAll();
+    List<T> getAllSortUsers();
+    List<T> getAllSortUsersByFamilyName();
+    List<T> getAllSortUsersByAge();
+    void removeUser(String fullName);
+    int getMaxFullName();
+    void buttonClickID();
+    T getById(int hum);
+    //List<T> getByNameGroup(String nameGroup);
+
+}
